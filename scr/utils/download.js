fba9e1ece624748f084e2f6458624f91e0a06e83
liulihong <1336240977@qq.com>
2018-04-18 11:18:03 +0800

    d

--- scr/utils/download.js
+++ scr/utils/download.js
@@ -6,27 +6,30 @@ import { connect } from "react-redux";
 var Zip = require('@remobile/react-native-zip');
 const downloadDest = utils.DOWNLOADDOCUMENTPATH; //下载之后保存的目录
 
-// class Downloader {
-//     constructor(callback) {
+// export default class Downloader {
+//     constructor(path, docName, callback) {
+//         this.path = path;
+//         this.docName = docName;
 //         this.callback = callback;
 //     }
+
 //     download() {
 //         let that = this;
 //         const progress = data => {
 //             const percentage = ((100 * data.bytesWritten) / data.contentLength) || 0;
 //             const text = `Progress ${percentage}%`;
-//             callback({ "path": that.path, "docName": docName, "status": "downloading", "progress": percentage.toFixed(0) + " %" });
+//             callback({ "path": that.path, "docName": that.docName, "status": "downloading", "progress": percentage.toFixed(0) + " %" });
 //             console.log(text);
 //         };
 //         const begin = res => {
-//             callback({ "path": path, "docName": docName, "status": "start", "progress": "0%" });
+//             callback({ "path": that.path, "docName": that.docName, "status": "start", "progress": "0%" });
 //             console.log('Download has begun');
 //         };
 //         const progressDivider = 1;
 
 //         const ret = RNFS.downloadFile({
-//             fromUrl: path,
-//             toFile: downloadDest + "/" + docName + ".zip",
+//             fromUrl: that.path,
+//             toFile: downloadDest + "/" + that.docName + ".zip",
 //             begin,
 //             progress,
 //             progressDivider
@@ -38,17 +41,40 @@ const downloadDest = utils.DOWNLOADDOCUMENTPATH; //下载之后保存的目录
 //             console.log("file download ");
 //             console.log(downloadDest);
 //             console.log(res);
-//             callback({ "path": path, "docName": docName, "status": "success", "progress": "解压中", unzip: "start" });
+//             callback({ "path": that.path, "docName": that.docName, "status": "success", "progress": "解压中", unzip: "start" });
 //             // 调用解压函数
-//             this.unzipNewCourse(docName);
+//             this.unzipNewCourse(that.docName);
 
 //         }).catch(err => {
 
-//             callback({ "path": path, "docName": docName, "status": "faild", "progress": "0%" });
+//             callback({ "path": that.path, "docName": that.docName, "status": "faild", "progress": "0%" });
 //             console.log(err)
 //             jobId = -1;
 //         });
 //     }
+
+//     unzipNewCourse(docName) {
+//         const oriPath = downloadDest + "/" + docName + ".zip";
+//         const newPath = downloadDest + "/" + docName;
+//         Zip.unzip(oriPath, newPath, (err) => {
+//             if (err) {
+//                 debugger
+//                 callback({ "path": path, "docName": docName, "status": "success", "progress": "0%", unzip: "faild" });
+//                 // 解压失败
+//                 console.log('error')
+//             }
+//             else {
+//                 // //解压成功，将zip删除
+//                 RNFS.unlink(oriPath).then(() => {
+//                     callback({ "path": path, "docName": docName, "status": "success", "progress": "100%", unzip: "success" });
+//                 });
+
+//                 console.log('success__newPath==' + newPath);
+//             }
+//         });
+//         jobId = -1;
+//     }
+
 // }
 
 // let downloader = new Downloader(adsf,adf);
